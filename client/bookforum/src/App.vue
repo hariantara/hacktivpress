8a75b49a5f304ed98a8223ad8d6821860b1790a1
hariantara <hariantara.iputu@gmail.com>
2017-09-14 14:35:54 +0700

    still not show on browser

--- client/bookforum/src/App.vue
+++ client/bookforum/src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <div class="">
+      <div class="row">
+        <div class="col-md-12">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Hello from '@/components/Hello'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Hello
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- client/bookforum/src/store.js
+++ client/bookforum/src/store.js
@@ -1,24 +1,28 @@
 import vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
-import router from './router/index'
+// import router from './router/index'
 
-vue.use('Vuex')
+vue.use(Vuex)
 
-exports default new Vuex.Store({
+export default new Vuex.Store({
   state: {
     userLogin: []
   },
   mutations: {
+    login (state, payload) {
+      state.userLogin = payload
+    }
   },
   actions: {
     login (store, payload) {
-      axios.post('http:3000/users/signin', {
+      axios.post('http://localhost:3000/users/signin', {
         username: payload.username,
         password: payload.password
       })
       .then((dataLogin) => {
-        console.log(dataLogin);
+        console.log(dataLogin)
+        store.commit('login', dataLogin.data)
       })
     }
   }
